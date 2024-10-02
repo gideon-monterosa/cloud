694dd488c9fa341c69a5c7630ffca9e67998eb46
gideon <gideon.monterosa@gmail.com>
2024-10-02 17:19:26 +0200

    add some steps

--- README.md
+++ README.md
@@ -7,11 +7,16 @@ Repository für das Cloud Computing Modul an der FHNW.
 **IPS**
 
 node01: 86.119.46.3
-node02: 
-node03: 
+node02: 86.119.44.187
+node03: 86.119.47.65
 
 connect to the server with:
-
 ```
 ssh debian@<ip>
 ```
+
+**Proxmox Webinterfaces**
+
+node01: https://86.119.46.3:8006/
+node02:
+node03:
--- plattform-1-proxmox/run-on-node.sh
+++ plattform-1-proxmox/run-on-node.sh
@@ -1,4 +1,30 @@
 # TODO Add the SSH key of our prof
 
+# See this link for more information about this part of the script
+# https://pve.proxmox.com/wiki/Install_Proxmox_VE_on_Debian_12_Bookworm#Install_Proxmox_VE
+
 # Add the Proxmox Repository
-echo "deb [arch=amd64] http://download.proxmox.com/debian/pve bookworm pve-no-subscription" >/etc/apt/sources.list.d/pve-install-repo.list
+echo "deb [arch=amd64] http://download.proxmox.com/debian/pve bookworm pve-no-subscription" | sudo tee /etc/apt/sources.list.d/pve-install-repo.list
+
+# Add the Proxmox VE repository key as root
+sudo wget https://enterprise.proxmox.com/debian/proxmox-release-bookworm.gpg -O /etc/apt/trusted.gpg.d/proxmox-release-bookworm.gpg
+# verify
+sha512sum /etc/apt/trusted.gpg.d/proxmox-release-bookworm.gpg
+# has to equal: 7da6fe34168adc6e479327ba517796d4702fa2f8b4f0a9833f5ea6e6b48f6507a6da403a274fe201595edc86a84463d50383d07f64bdde2e3658108db7d6dc87 /etc/apt/trusted.gpg.d/proxmox-release-bookworm.gpg
+
+# Update your repository and system by running
+sudo apt update && sudo apt full-upgrade
+
+# Install the Proxmox VE Kernel
+sudo apt install proxmox-default-kernel
+sudo systemctl reboot
+
+# Install the Proxmox VE packages
+sudo apt install proxmox-ve postfix open-iscsi chrony
+
+# Remove the Debian Kernel
+sudo apt remove linux-image-amd64 'linux-image-6.1*'
+sudo update-grub
+
+# Recommended: Remove the os-prober Package
+sudo apt remove os-prober
