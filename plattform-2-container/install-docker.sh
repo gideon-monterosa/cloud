fcb8431016a7cf1dbc616ec7292ff94dfb172699
gideon <gideon.monterosa@gmail.com>
2024-11-20 17:58:57 +0100

    setup and run monitoring using docker compose

--- plattform-2-container/install-docker.sh
+++ plattform-2-container/install-docker.sh
@@ -12,10 +12,18 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 
-// install docker packages
+# install docker packages
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-// install and start the monitoring stack
+# install and start the monitoring stack
 sudo apt install git
 git clone https://gitlab.fhnw.ch/cloud/cloud/monitoringstack.git
 cd monitorstack
+
+sudo docker compose up -d
+
+# check if the stack is running correctly
+sudo docker ps
+
+# add required security groups (9090, 9100, 8080, 9113)
+
