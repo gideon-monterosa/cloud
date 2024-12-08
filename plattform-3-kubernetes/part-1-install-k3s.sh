8726827ea534cb8b8e0a9d68bb394e458ba80e45
Shane Zulauf <shane.zulauf@students.fhnw.ch>
2024-12-08 15:23:44 +0100

    added eval user

--- plattform-3-kubernetes/part-1-install-k3s.sh
+++ plattform-3-kubernetes/part-1-install-k3s.sh
@@ -1,7 +1,30 @@
 # Add shanes ssh key
 echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDtMSm3Vi0ReI7Bv6vJCv6d4XjCobQ2MebIWk1rmKrP1aZInVoU+Z5NUVKn/Ze4Gqs0wjvbVBTVBaGBG+1z/DJRDmyqHqJ29lSvGfQqsXenTs5LqnXYHaNFUbqrxcWBGmYOcRjbWx+1tIFOs/LRFfM+74HJcCcVde6L6T3DLB6HsWrtnTDDXDoTJbxNVzjDcmft6bxvm3mS3L8ZHpmcgKVQLdlQLTfCXsXJzZyRQ37Bc1hc1c/t7KEphGgzpAr0BM+t/Rffcqq57gyrIfLseb1PIPTwJ1qjePcAhchvVV4YsbtFJJb9JMX6rqi+t9NnlXuCwXF8ulZaTaJ7UkijQqjw78m7jLz2eQaMQTiZJ/0rJGb/usmUpWsfoESyFuSWfMf0DzWuomare6oAYVaFJbT4zdW/u664nrN51M1fahc8zWCzmMxee4cvjNA8ehjDYB5TrbV55IW01LLo/HFDnRzxOiB4l8m9XLtkqn3GxEp/hyTT+9Cw2WXFpvQufHWXEz8= root@PimpJuice" >> ~/.ssh/authorized_keys
 
-# TODO: Add eval users
+# Add the SSH key of our prof
+echo 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKMf2NalRNgiv1bPjzF+4R4bak81D4SP7vvb0F7KeE7D sebastiangraf@laptop' | sudo tee -a ~/.ssh/authorized_keys > /dev/null
+
+# Add eval user
+# password 1234
+sudo adduser eval
+
+# Allow passwordless sudo privleges (add following to sudoers file)
+sudo visudo
+: '
+eval ALL=(ALL) NOPASSWD: ALL
+'
+#switch to eval user
+sudo su eval
+
+# create and Add ssh key to evalUser authorized_keys file
+mkdir -p ~/.ssh
+
+chmod 700 ~/.ssh
+
+touch ~/.ssh/authorized_keys
+
+chmod 600 ~/.ssh/authorized_keys
+
 
 # k3s auf dem k3snode01 installieren (master)
 sudo apt update && sudo apt upgrade -y
